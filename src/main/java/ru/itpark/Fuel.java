a3b40d7a010f95a1340c953ad5362e8d8f2c0240
dilyarakhabibullina <khdilyara@rambler.ru>
2019-02-27 23:26:32 +0300

    code for fuel

---
+++ src/main/java/ru/itpark/Fuel.java
@@ -0,0 +1,11 @@
+package ru.itpark;
+
+public class Fuel {
+    public static void main(String[] args) {
+        int fuelCons=16;// расход топлива на 100 км //
+        int fuelAmount=60;//наличие топлива//
+        int distance=fuelAmount/fuelCons;
+        System.out.println(distance); // расстояние
+    }
+
+}
